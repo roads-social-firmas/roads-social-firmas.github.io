828dc21a1f96c4c8350c8004ca28b0afdb3c5b8c
Danny Engelman <danny@engelman.nl>
2022-11-28 19:57:01 +0100

    add cachebuster on load file

--- components/home-page.js
+++ components/home-page.js
@@ -7,6 +7,7 @@ customElements.define("home-page", class extends HTMLElement {
                         let gitURI = `https://github.com/roads-social-firmas/roads-social-firmas.github.io/edit/main/`
                         let socialFirm = document.querySelector("meta[name=socialdirectory]").content;
                         let file = lf.getAttribute("src");
+                        file += `?` + (new Date()/1);
                         let newWindow = window.open(gitURI + socialFirm + `/` + file, '_blank');
                         newWindow.focus();
                     } else {
