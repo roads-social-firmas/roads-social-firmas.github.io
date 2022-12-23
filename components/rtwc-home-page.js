f36765346462aac8e2cdf309555673eef2fe55e8
Danny Engelman <danny@engelman.nl>
2022-12-23 16:53:23 +0100

    added RT website traffic testing

--- components/rtwc-home-page.js
+++ components/rtwc-home-page.js
@@ -1,25 +1,33 @@
-customElements.define("home-page", class extends HTMLElement {
+customElements.define(
+  "home-page",
+  class extends HTMLElement {
     connectedCallback() {
-        setTimeout(() => {
+      setTimeout(() => {
+        // add a CTRL+click event to load-file elements, opening the Source Code in a new tab
+        this.querySelectorAll("load-file").forEach((lf) => {
+          lf.addEventListener("click", (e) => {
+            if (e.ctrlKey) {
+              let gitURI = `https://github.com/roads-social-firmas/roads-social-firmas.github.io/edit/main/`;
+              let socialFirm = document.querySelector(
+                "meta[name=socialdirectory]"
+              ).content;
+              let file = lf.getAttribute("src");
+              let uri = gitURI + socialFirm + `/` + file;
+              uri += `?` + new Date() / 1; // add cache buster to uri
+              let newWindow = window.open(uri, "_blank");
+              newWindow.focus();
+            } else {
+            }
+          });
+        });
 
-            // add a CTRL+click event to load-file elements, opening the Source Code in a new tab
-            this.querySelectorAll("load-file").forEach((lf) => {
-                lf.addEventListener("click", (e) => {
-                    if (e.ctrlKey) {
-                        let gitURI = `https://github.com/roads-social-firmas/roads-social-firmas.github.io/edit/main/`
-                        let socialFirm = document.querySelector("meta[name=socialdirectory]").content;
-                        let file = lf.getAttribute("src");
-                        file += `?` + (new Date()/1);
-                        let newWindow = window.open(gitURI + socialFirm + `/` + file, '_blank');
-                        newWindow.focus();
-                    } else {
-                    }
-                });
-            });
-
-            // simulate website traffic
-            fetch("https://roadsmap.nl");
-
-        }, 100);
+        // simulate website traffic on your test VPS server
+        fetch("https://roadsmap.nl", {
+          method: "GET",
+          mode: "no-cors",
+        });
+        // simulate website traffic on your test VPS server
+      }, 100);
     }
-})
+  }
+);
